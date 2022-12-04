34409f8e6fb43e6db2e62fa5665921cd6e76dde0
bulik0071 <jakub.odziemczyk95@gmail.com>
2022-12-04 10:22:17 +0100

    Fourth day AOC

---
+++ day4/day4.py
@@ -0,0 +1,37 @@
+import re
+filename='puzzleinput.txt'
+def importpuzzles(filename):
+    file=open(filename)
+    rawpuzzleinput=list(file)
+    lines=[]
+    for line in rawpuzzleinput:
+        lines.append(re.sub('\n', '', line).split(' '))
+    return lines
+def prepareLine(inputArray):
+    outputArray=[]
+    i=''
+    for line in inputArray:
+        i=line[0].replace('-','#').replace(',','#')
+        outputArray.append(i.split('#'))
+    return outputArray
+def checkifcontains(inputArray):
+    if int(inputArray[0])>=int(inputArray[2]) and int(inputArray[1])<=int(inputArray[3]) or int(inputArray[2])>=int(inputArray[0]) and int(inputArray[3])<=int(inputArray[1]):
+        return True
+    else:
+        return False
+        """                                                                                                                                                                                                                                     5-7,7-9"""
+def checkifoverlap(inputArray):
+    if int(inputArray[0])>=int(inputArray[2]) and int(inputArray[0])<=int(inputArray[3]) or int(inputArray[1])>=int(inputArray[2]) and int(inputArray[1])<=int(inputArray[3]) or int(inputArray[2])>=int(inputArray[0]) and int(inputArray[2])<=int(inputArray[1]) or int(inputArray[3])>=int(inputArray[0]) and int(inputArray[3])<=int(inputArray[0]):
+        return True
+    else:
+        return False
+myArray=prepareLine(importpuzzles(filename))
+assingments=0
+overlaps=0
+for element in myArray:
+    if checkifcontains(element):
+        assingments=assingments+1
+    if checkifoverlap(element):
+        overlaps=overlaps+1
+print(f"Total assignments: {assingments}")
+print(f"Total overlaps: {overlaps}")
